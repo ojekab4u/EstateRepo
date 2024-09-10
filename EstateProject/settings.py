924dfb634e50486cce648874f4bc514453f40346
unknown <ojekab2580@gmail.com>
2024-09-10 03:25:16 -0700

    still first commit

--- EstateProject/settings.py
+++ EstateProject/settings.py
@@ -148,13 +148,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = config("EMAIL_HOST", cast=str)
-EMAIL_PORT = config("EMAIL_PORT", cast=str, default='587')
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", cast=str)
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = config("EMAIL_HOST", cast=str)
+# EMAIL_PORT = config("EMAIL_PORT", cast=str, default='587')
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+# EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+# DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", cast=str)
 # EMAIL_USE_SSL = config("EMAIL_USE_TLS", cast=bool, default=False)
 
 # ADMIN_USER_NAME = 
