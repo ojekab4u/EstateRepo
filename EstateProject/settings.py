bb0c69cabc594260cfd9a26f7b16a567c134f846
unknown <ojekab2580@gmail.com>
2024-09-15 04:05:18 -0700

    updating the estimation as well as deployement requirments

--- EstateProject/settings.py
+++ EstateProject/settings.py
@@ -134,17 +134,19 @@ USE_TZ = True
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Static files configuration (Optional if using static files)
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = BASE_DIR/ 'staticfiles'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
+if DEBUG:
+    STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+else:
+    STATIC_ROOT = BASE_DIR/ 'staticfiles'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
 
 
 # Default primary key field type
