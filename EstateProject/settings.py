b0985d320bdd1d4ff5ec7451b039460a62018333
ojekab4u <ojekab2580@gmail.com>
2026-09-01 16:19:06 +0100

    Cleaning up

--- EstateProject/settings.py
+++ EstateProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-j8ymzm7nn&_+9qbk$m9va4sh%24i!7m(-%f3)_88&b(g7c#8fu
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
-DEBUG = False
+DEBUG = True
 # if ALLOWED_HOSTS == ['']:
 #     DEBUG = True
 
